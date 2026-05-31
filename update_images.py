0ae6dee4efb56cd0e1ce292c3e7799ae4a6336fc
kwikwag <392199+kwikwag@users.noreply.github.com>
2026-05-31 16:48:17 +0300

    update onboarding images

--- update_images.py
+++ update_images.py
@@ -2,12 +2,27 @@ import os
 import base64
 import glob
 
-def main():
-    os.makedirs('imgs', exist_ok=True)
-    os.makedirs('apps-script/src/imgs-encoded', exist_ok=True)
+CONTENT_TYPES = {
+    "png": "image/png",
+    "jpg": "image/jpeg",
+    "jpeg": "image/jpeg",
+    "webp": "image/webp",
+    "git": "image/gif",
+}
 
-    # Encode all images in imgs/ to base64 Data URIs in apps-script/src/imgs-encoded/
-    image_paths = glob.glob('imgs/*.png') + glob.glob('imgs/*.jpg') + glob.glob('imgs/*.jpeg') + glob.glob('imgs/*.gif')
+SRC_DIR = 'imgs'
+TARGET_DIR = 'apps-script/server/imgs-encoded'
+
+def main():
+    os.makedirs(SRC_DIR, exist_ok=True)
+    os.makedirs(TARGET_DIR, exist_ok=True)
+
+    # Encode all images in imgs/ to base64 Data URIs in TARGET_DIR
+    image_paths = [
+        path
+        for ext in CONTENT_TYPES
+        for path in glob.glob(f'imgs/*.{ext}')
+    ]
     for filepath in image_paths:
         filename = os.path.basename(filepath)
         name, ext = os.path.splitext(filename)
@@ -16,17 +31,11 @@ def main():
         with open(filepath, "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
         
-        mime = "image/png"
-        if ext in [".jpg", ".jpeg"]:
-            mime = "image/jpeg"
-        elif ext == ".gif":
-            mime = "image/gif"
-        elif ext == ".svg":
-            mime = "image/svg+xml"
+        mime = CONTENT_TYPES[filepath.rsplit(".", 1)[-1]]
             
         data_uri = f"data:{mime};base64,{encoded_string}"
         
-        out_filepath = os.path.join('apps-script/src/imgs-encoded', f"{name}.html")
+        out_filepath = os.path.join(TARGET_DIR, f"{name}.html")
         with open(out_filepath, "w") as out_file:
             out_file.write(data_uri)
         print(f"Encoded {filename} to {out_filepath}")
